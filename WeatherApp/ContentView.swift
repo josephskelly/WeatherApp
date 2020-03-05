2a257c3a10e4124d1b8b13b7f34be0d9006b7c78
Joseph Kelly <jskelly84@gmail.com>
2020-03-05 15:48:29 -0800

    Initial commit.

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Joseph Kelly on 3/4/20.
+//  Copyright © 2020 BadMotivator. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Weather!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/README.md
@@ -0,0 +1,9 @@
+#  README.md
+
+Weather! Its an app!
+
+**Purpose**
+
+Provide weather updates via notifications. After setup, the user should not have to open the app to check the weather. App will deliver important info when necessary.
+
+
